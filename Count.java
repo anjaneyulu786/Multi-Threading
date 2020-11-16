8bb984359f688b38a1f3abff1860ee3cc9d10027
anjaneyulu786 <47409569+anjaneyulu786@users.noreply.github.com>
2020-11-16 11:44:37 +0530

    Update Count.java

--- Count.java
+++ Count.java
@@ -6,7 +6,7 @@ class Count extends Thread
      System.out.println ("my thread created" + this);
      start ();
    }
-   public void run() {
+   public void run () {
      try {
         for (int i=0 ;i<10;i++) {
            System.out.println ("Printing the count " + i);
@@ -27,13 +27,13 @@ class ExtendingExample
       Count cnt = new Count ();
       try
       {
-         while(cnt.isAlive ())
+         while (cnt.isAlive ())
          {
            System.out.println ("Main thread will be alive till the child thread is live");
            Thread.sleep (1500);
          }
       }
-      catch(InterruptedException e)
+      catch (InterruptedException e)
       {
         System.out.println ("Main thread interrupted");
       }
